7ac202a23d3ffb3bb10f6316513a2124110fe522
Hacsu8166Alt <149301530+Hacsu8166Alt@users.noreply.github.com>
2023-11-07 20:04:19 +0500

    Create instructions.lua

---
+++ instructions.lua
@@ -0,0 +1,11 @@
+first go to gameguardian.net
+then download the latest version of it.
+then install it.
+
+google play store will say ITS DANGEROUS. but its not. its an exploit thats why its saying thet.
+
+then download blockman go from the playstore/appstore
+
+then get CLONEApp by mohg bg, then add everything to it.
+
+---thanks
